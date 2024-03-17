7ccb5acb4183f19b43dc862e8df7de2008c33db1
nijatzamanov <nijat.z@code.edu.az>
2024-03-17 23:06:33 +0400

    fix

--- Chrome Extension 1/background.js
+++ Chrome Extension 1/background.js
@@ -1,23 +1,30 @@
-alert("Hi")
 
 function MyGenericClick(info, tab) {
-    console.log("Clicked on page:", info, tab);
+    console.log("Clicked an image:", info, tab);
     chrome.windows.create({
         "url": "https://www.facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
         "type": "popup"
     })
 }
 
-chrome.contextMenus.create ({
-    "title": "Share",
-    "contexts": ["page"],
-    "onclick": MyGenericClick
-})
+function MyQuoteClick(info, tab) {
+    console.log("Clicked on page:", info, tab);
+    chrome.windows.create({
+        "url": "https://www.facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
+        "type": "popup"
+    })
+}
 
-chrome.contextMenus.create({
+chrome.contextMenus.create ({
     "title": "Share Image",
     "contexts": ["image"],
     "onclick": MyImageClick
+})
+
+chrome.contextMenus.create({
+    "title": "Share Quote",
+    "contexts": ["selection"],
+    "onclick": MyQuoteClick
 })
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
