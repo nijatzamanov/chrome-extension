b255900602048454cf0868a1be6938ef890e9b70
nijatzamanov <nijat.z@code.edu.az>
2024-03-18 07:53:27 +0400

    some issues fixed in Chrome Extension 1

--- Chrome Extension 1/background.js
+++ Chrome Extension 1/background.js
@@ -1,25 +1,29 @@
 
-function MyImageClick(info, tab) {
-    console.log("Clicked an image:", info, tab);
-    /* chrome.windows.create({
-        "url": "https://www.facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+function MyImageClick(info, tab){
+    console.log("Clicked an image", info, tab);
+    /*
+        chrome.windows.create({
+            "url": "https://facebook.com/sharer.php?u=" + info.srcUrl + "&display=popup",
+            "type": "popup"
+        })
+    */
+chrome.downloads.download({"url": info.srcUrl})
+}
+
+function MyQuoteClick(info, tab){
+    console.log("Clicked an image", info, tab);
+    /*
+    chrome.windows.create({
+        "url": "https://facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
         "type": "popup"
-    }) */
-    chrome.downloads.download({
-        "url": info.srcUrl
     })
+    */
+   chrome.downloads.download(info.srcUrl)
+
+
 }
 
-function MyQuoteClick(info, tab) {
-    console.log("Clicked on page:", info, tab);
-    /* chrome.windows.create({
-        "url": "https://www.facebook.com/sharer.php?u=" + info.pageUrl + "&display=popup&quote=" + info.selectionText,
-        "type": "popup"
-    }) */
-    chrome.downloads.download(info.srcUrl)
-}
-
-chrome.contextMenus.create ({
+chrome.contextMenus.create({
     "title": "Share Image",
     "contexts": ["image"],
     "onclick": MyImageClick
@@ -31,9 +35,9 @@ chrome.contextMenus.create({
     "onclick": MyQuoteClick
 })
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse){
     console.log("message", msg)
-    sendResponse({
-        "text": "Received the links"
-    })
+    sendResponse({"text": "Received the links"});
+
 })
--- Chrome Extension 1/index.js
+++ Chrome Extension 1/index.js
@@ -1,23 +1,19 @@
 console.log("My extension")
 
-// var dom = document.getElementById("rso");
 var links = document.getElementsByTagName("a");
 var formatted_links = [];
 
-for (let i = 0; i < links.length; i++) {
+for(let i = 0; i < links.length; i++){
     let title = links[i].text;
     let href = links[i].href;
 
-    if (title !== "" && href !== "") {
-        formatted_links.push({
-            title: title,
-            href: href
-        })
+    if(title !== "" && href !== ""){
+        formatted_links.push({title: title, href: href});
     }
 }
 
 chrome.runtime.sendMessage({
-    "action": "print_messages",
+    "action": "print_messages", 
     "data": formatted_links
 }, res => {
     console.log(res);
